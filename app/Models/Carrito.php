6333fe9b00e53ecdd3cc2c0f818e4b479163ab54
Adrick <adriel.totora@tecsup.edu.pe>
2025-11-22 00:59:18 -0500

    Update Carrito.php

--- app/Models/Carrito.php
+++ app/Models/Carrito.php
@@ -9,4 +9,16 @@ class Carrito extends Model
     protected $table = 'carrito';
     protected $primaryKey = 'id_carrito';
     public $timestamps = false;
+
+    protected $fillable = [
+        'id_usuario',
+        'id_telefono',
+        'cantidad',
+        'fecha_agregado',
+    ];
+
+    public function telefono()
+    {
+        return $this->belongsTo(Telefono::class, 'id_telefono', 'id_telefono');
+    }
 }
